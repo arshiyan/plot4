8aca2efed41cd2567f3534281d1b94a13fddff66
Mohammad Javad Arshiyan <mj.arshiyan@gmail.com>
2022-11-26 17:57:24 +0330

    bug fix and add test data for draw board

--- boardClass.php
+++ boardClass.php
@@ -6,10 +6,21 @@ class BoardClass
     public $row;
     public $columns;
 
+    public $playPosition;
+    private $validitor;
+
     function __construct($rows, $columns)
     {
         $this->rows = $rows;
         $this->columns = $columns;
+
+        $this->validitor = new Validetor();
+
+        $this->playPosition[$this->rows-1][$this->columns-2] = "*";
+        $this->playPosition[$this->rows-2][$this->columns-4] = "o";
+        $this->playPosition[$this->rows-4][$this->columns-2] = "*";
+        $this->playPosition[$this->rows-3][$this->columns-2] = "o";
+
     }
 
     public function getRow()
@@ -29,29 +40,45 @@ class BoardClass
 
     }
 
+    public function runTimeDraw($row,$col)
+    {
+        if($this->validitor->checkNotEmpty($this->playPosition[$row][$col]))
+        {
+            echo " ".$this->playPosition[$row][$col];
+        }
+        else
+        {
+            //echo $row.$col;
+            echo "  ";
+        }
+    }
+
+
     //draw box
     public function boxDraw()
     {
 
-        for ($x = 0; $x <= $this->columns; $x++) {
-            echo(" $x ");
+        for ($x = 1; $x <= $this->columns; $x++) {
+            echo " ".($x)." " ;
         }
         echo "\n";
 
-        for ($i = 0; $i < $this->rows; $i++) {
-            for ($x = 0; $x <= $this->columns; $x++) {
-                echo "║" . $i . $x;
-
+        for ($i = 1; $i <= $this->rows; $i++) {
+            for ($x = 1; $x <= $this->columns; $x++) {
+                echo "║";
+                $this->runTimeDraw($i,$x);
                 if ($x == $this->columns) {
-                    echo "║";
+                    echo "║ ";
                 }
             }
+
+            echo " ".($i);
             echo "\n";
         }
 
         //draw footer
         echo "╚═";
-        for ($x = 0; $x <= $this->columns - 1; $x++) {
+        for ($x = 1; $x <= $this->columns - 1; $x++) {
             //echo (" ╩ ");
             echo("═╩═");
         }
@@ -91,4 +118,5 @@ class BoardClass
 
 
     }
+
 }
